1edf3ec6804fefd5f90ebdd3d2ef027bf7f77d52
Makrennel <makrommel@protonmail.ch>
2024-08-16 23:45:48 +0100

    Try escape?

--- disko.nix
+++ disko.nix
@@ -2,7 +2,7 @@
   pkgs ? import <nixpkgs> {},
   device ? throw "Set this to the disk device you want to use, e.g. /dev/nvme0n1 or /dev/vda",
   efi-size ? "512M", # 1/2 a GiB by default 
-  swap-size ? builtins.readFile (pkgs.runCommand "get-swap-size" {} "grep MemTotal /proc/meminfo | sed 's/[^0-9]*//g' | read mem; echo $[ $mem / 1024 ]'M' > $out"), # Same size as RAM by default
+  swap-size ? builtins.readFile (pkgs.runCommand "get-swap-size" {} "grep MemTotal /proc/meminfo | sed 's/[^0-9]*//g' | read mem; echo \"$[ $mem / 1024 ]'M'\" > $out"), # Same size as RAM by default
   main-size ? "100%", # Use 100% of remaining space by default
   ...
 }: {
