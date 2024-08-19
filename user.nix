ebc13a1673d92bf85b935a97b3450c3fa088c92a
Makrennel <makrommel@protonmail.ch>
2024-08-20 00:21:18 +0100

    update

--- user.nix
+++ user.nix
@@ -23,44 +23,42 @@ in {
       isNormalUser = true;
     };
 
-    home-manager = {
-      users.user = import ./home;
-      sharedModules = [
-        ({ config, lib, ... }: {
-          home.file.".bin".source = config.lib.file.mkOutOfStoreSymlink "bin";
-          home.file.".cache".source = config.lib.file.mkOutOfStoreSymlink "cache";
-          home.file.".config".source = config.lib.file.mkOutOfStoreSymlink "config";
-          home.file.".local".source = config.lib.file.mkOutOfStoreSymlink ".";
-          home.file.".var".source = config.lib.file.mkOutOfStoreSymlink "var";
+    home-manager.users."${config.makrenos.username}" = import ./home;
+    home-manager.sharedModules = [
+      ({ config, lib, ... }: {
+        home.file.".bin".source = config.lib.file.mkOutOfStoreSymlink "bin";
+        home.file.".cache".source = config.lib.file.mkOutOfStoreSymlink "cache";
+        home.file.".config".source = config.lib.file.mkOutOfStoreSymlink "config";
+        home.file.".local".source = config.lib.file.mkOutOfStoreSymlink ".";
+        home.file.".var".source = config.lib.file.mkOutOfStoreSymlink "var";
 
-          home.file.".fonts".source = config.lib.file.mkOutOfStoreSymlink "share/fonts";
-          home.file.".icons".source = config.lib.file.mkOutOfStoreSymlink "share/icons";
-          home.file.".themes".source = config.lib.file.mkOutOfStoreSymlink "share/themes";
+        home.file.".fonts".source = config.lib.file.mkOutOfStoreSymlink "share/fonts";
+        home.file.".icons".source = config.lib.file.mkOutOfStoreSymlink "share/icons";
+        home.file.".themes".source = config.lib.file.mkOutOfStoreSymlink "share/themes";
 
-          xdg.cacheHome = "${config.home.homeDirectory}/cache";
-          xdg.configHome = "${config.home.homeDirectory}/config";
-          xdg.dataHome = "${config.home.homeDirectory}/share";
-          xdg.stateHome = "${config.home.homeDirectory}/state";
+        xdg.cacheHome = "${config.home.homeDirectory}/cache";
+        xdg.configHome = "${config.home.homeDirectory}/config";
+        xdg.dataHome = "${config.home.homeDirectory}/share";
+        xdg.stateHome = "${config.home.homeDirectory}/state";
 
-          xdg.userDirs.enable = true;
-          xdg.userDirs.desktop = "${config.home.homeDirectory}/home/Desktop";
-          xdg.userDirs.documents = "${config.home.homeDirectory}/home/Documents";
-          xdg.userDirs.pictures = "${config.home.homeDirectory}/home/Images";
-          xdg.userDirs.music = "${config.home.homeDirectory}/home/Music";
-          xdg.userDirs.publicShare = "${config.home.homeDirectory}/home/Shared";
-          xdg.userDirs.templates = "${config.home.homeDirectory}/home/Templates";
-          xdg.userDirs.videos = "${config.home.homeDirectory}/home/Videos";
-          xdg.userDirs.extraConfig = {
-            XDG_REPOSITORY_DIR = "${config.home.homeDirectory}/home/Repositories";
-            XDG_USER_HOME = "${config.home.homeDirectory}/home";
-            XDG_VAR_HOME = "${config.home.homeDirectory}/var";
-            XDG_BIN_HOME = "${config.home.homeDirectory}/bin";
-          };
+        xdg.userDirs.enable = true;
+        xdg.userDirs.desktop = "${config.home.homeDirectory}/home/Desktop";
+        xdg.userDirs.documents = "${config.home.homeDirectory}/home/Documents";
+        xdg.userDirs.pictures = "${config.home.homeDirectory}/home/Images";
+        xdg.userDirs.music = "${config.home.homeDirectory}/home/Music";
+        xdg.userDirs.publicShare = "${config.home.homeDirectory}/home/Shared";
+        xdg.userDirs.templates = "${config.home.homeDirectory}/home/Templates";
+        xdg.userDirs.videos = "${config.home.homeDirectory}/home/Videos";
+        xdg.userDirs.extraConfig = {
+          XDG_REPOSITORY_DIR = "${config.home.homeDirectory}/home/Repositories";
+          XDG_USER_HOME = "${config.home.homeDirectory}/home";
+          XDG_VAR_HOME = "${config.home.homeDirectory}/var";
+          XDG_BIN_HOME = "${config.home.homeDirectory}/bin";
+        };
 
-          home.preferXdgDirectories = true;
-        })
-      ];
-    };
+        home.preferXdgDirectories = true;
+      })
+    ];
   };
 }
 
